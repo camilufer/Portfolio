48aa55cc127e2671bf2f7b069d38e2216efc75af
camilufer <camilu.fer21@gmail.com>
2018-03-22 00:40:14 -0300

    añadiendo navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,15 @@ $(document).ready(function(){
    event.preventDefault();
     })
 
+   
+            var sideslider = $('[data-toggle=collapse-side]');
+            var sel = sideslider.attr('data-target');
+            var sel2 = sideslider.attr('data-target-2');
+            sideslider.click(function(event){
+                $(sel).toggleClass('in');
+                $(sel2).toggleClass('out');
+            });
+      
   
 
   // Select all links with hashes
