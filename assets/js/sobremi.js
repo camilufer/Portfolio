614ab2ab16d3ac31a06e18e5f69726fe6dfdef75
camilufer <camilu.fer21@gmail.com>
2018-03-31 04:56:06 -0300

    estilos

--- assets/js/sobremi.js
+++ assets/js/sobremi.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 // Typing Function
 
 const writeText = () => {
-  let text = 'Hola, extrano... Mi nombre es Camila, tengo 28 años y soy de Valparaíso, mi historia con el código comenzó apenas el 2017, pero mis ganas de saber me han llevado a aprender más de lo que imaginaba!. En mis tiempos libres me gusta jugar video juegos, pasar largas horas viendo series y estar con mis gatitas <3. Me gusta mucho aprender y estoy segura que mi motivación y disciplina me llevarán a lograr grandes cosas :)';
+  let text = 'Hola, extraño... Mi nombre es Camila, tengo 28 años y soy de Valparaíso, mi historia con el código comenzó apenas el 2017, pero mis ganas de saber me han llevado a aprender más de lo que imaginaba!. En mis tiempos libres me gusta jugar video juegos, pasar largas horas viendo series y estar con mis gatitas <3. Me gusta mucho aprender y estoy segura que mi motivación y disciplina me llevarán a lograr grandes cosas :)';
   const typing = new Typing(text, 50);
   typing();
   $('#main-container').append('<span id="blinker">|</span>');
