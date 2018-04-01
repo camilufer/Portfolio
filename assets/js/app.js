ee47a00175224aa8c3b7788cc8771933c428cdb9
camilufer <camilu.fer21@gmail.com>
2018-04-01 12:33:41 -0300

    final desktop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,17 @@
-
+/*slider*/
  
-
-   
-            var sideslider = $('[data-toggle=collapse-side]');
-            var sel = sideslider.attr('data-target');
-            var sel2 = sideslider.attr('data-target-2');
-            sideslider.click(function(event){
-                $(sel).toggleClass('in');
-                $(sel2).toggleClass('out');
-            });
+var sideslider = $('[data-toggle=collapse-side]');
+var sel = sideslider.attr('data-target');
+var sel2 = sideslider.attr('data-target-2');
+sideslider.click(function(event){
+    $(sel).toggleClass('in');
+    $(sel2).toggleClass('out');
+});
       
-
-   $(".dog").mouseover(function(){
-        $(".dog-msj").removeClass("hide");
-    });
+/*msj prrito*/
+$(".dog").mouseover(function(){
+$(".dog-msj").removeClass("hide");
+});
 
 
 
--- assets/js/sobremi.js
+++ assets/js/sobremi.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 // Typing Function
 
 const writeText = () => {
-  let text = 'Hola, extraño... Mi nombre es Camila, tengo 28 años y soy de Valparaíso, mi historia con el código comenzó apenas el 2017, pero mis ganas de saber me han llevado a aprender más de lo que imaginaba!. En mis tiempos libres me gusta jugar video juegos, pasar largas horas viendo series y estar con mis gatitas <3. Me gusta mucho aprender y estoy segura que mi motivación y disciplina me llevarán a lograr grandes cosas :)';
+  let text = 'Hola, extraño... Mi nombre es Camila, tengo 28 años y soy de Valparaíso, mi historia con el código comenzó apenas el 2017 en Laboratoria, donde adquirí conocimientos sobre lenguajes de programación y metodologías ágiles, pero mis ganas de saber me han llevado a aprender más de lo que imaginaba!. En mis tiempos libres me gusta jugar video juegos, ver series y disfrutar con mis amigos <3. <br> Me gusta mucho aprender por mi misma; la disciplina, determinación y el feedback son claves para el éxito. :)';
   const typing = new Typing(text, 50);
   typing();
   $('#main-container').append('<span id="blinker">|</span>');
