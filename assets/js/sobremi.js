1b80faac676028183776e72d119a2fcb05b68d23
camilufer <camilu.fer21@gmail.com>
2018-04-01 19:49:49 -0300

    añadiendo iconos

--- assets/js/sobremi.js
+++ assets/js/sobremi.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 // Typing Function
 
 const writeText = () => {
-  let text = 'Hola, extraño... Mi nombre es Camila, tengo 28 años y soy de Valparaíso, mi historia con el código comenzó apenas el 2017 en Laboratoria, donde adquirí conocimientos sobre lenguajes de programación y metodologías ágiles, pero mis ganas de saber me han llevado a aprender más de lo que imaginaba!. En mis tiempos libres me gusta jugar video juegos, ver series y disfrutar con mis amigos <3. <br> Me gusta mucho aprender por mi misma; la disciplina, determinación y el feedback son claves para el éxito. :)';
+  let text = 'Hola, Mi nombre es Camila, tengo 28 años y soy de Valparaíso. <br> Mi historia con el código comenzó apenas el 2017 en <a href="http://www.laboratoria.la/">Laboratoria</a>, donde adquirí conocimientos sobre lenguajes de programación y metodologías ágiles, pero mis ganas de saber me han llevado a aprender más de lo que imaginaba!. En mis tiempos libres me gusta jugar video juegos, ver series y disfrutar con mis amigos <3. <br> Me gusta mucho aprender por mi misma; la disciplina y la constancia son claves para el éxito. :)';
   const typing = new Typing(text, 50);
   typing();
   $('#main-container').append('<span id="blinker">|</span>');
